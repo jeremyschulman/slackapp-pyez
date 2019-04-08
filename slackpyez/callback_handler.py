0cd007769b8997ba3e1418bbe177b7adf7739ba5
Jeremy Schulman <jeremy.schulman@mlb.com>
2019-04-07 21:06:32 -0400

    done

--- slackpyez/callback_handler.py
+++ slackpyez/callback_handler.py
@@ -27,17 +27,16 @@ class CallbackHandler(dict):
         callback = self.get(key_val)
 
         if not callback:
-            raise ValueError('Attempting to use key-value {}, no registered handler'.format(
-                key_val))
+            raise ValueError(f'Attempting to use key-value {key_val}, no registered handler')
 
         return callback
 
-    def __call__(self, key_item, **kwargs):
-        key_val = self._get_key(key_item)
-        callback = self.get(key_val)
-
-        if not callback:
-            raise ValueError('Attempting to use key-value {}, no registered handler'.format(
-                key_val))
-
-        return callback(**kwargs)
+    # def __call__(self, key_item, **kwargs):
+    #     key_val = self._get_key(key_item)
+    #     callback = self.get(key_val)
+    #
+    #     if not callback:
+    #         raise ValueError('Attempting to use key-value {}, no registered handler'.format(
+    #             key_val))
+    #
+    #     return callback(**kwargs)
