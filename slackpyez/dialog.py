6b4a02e30b12a78395ae3d2e3f6c86a52588c08d
Jeremy Schulman <jeremy.schulman@mlb.com>
2019-04-13 10:53:16 -0400

    adding ux_dialog

--- slackpyez/dialog.py
+++ slackpyez/dialog.py
@@ -27,33 +27,7 @@ class SlackDialog(dict):
         self['state'] = rqst.state
         self.trigger_id = rqst.trigger_id
 
-    @property
-    def on_submit(self):
-        return self._on_submit_func
 
-    @on_submit.setter
-    def on_submit(self, func):
-        self.rqst.app.register_dialog_submit(self.callback_id, func)
-
-    @staticmethod
-    def i_option(label, value):
-        return dict(label=label, value=value)
-
-    @staticmethod
-    def e_text(label, name, **kwargs):
-        return {'type': 'text', 'label': label, 'name': name,
-                **kwargs}
-
-    @staticmethod
-    def e_select(text, name, options=None, option_groups=None, **kwargs):
-        if options:
-            kwargs['options'] = options
-        elif option_groups:
-            kwargs['option_groups'] = option_groups
-        else:
-            raise RuntimeError("Missing param 'options' or 'option_groups'.")
-
-        return {'type': 'select', 'label': text, 'name': name, **kwargs}
 
     def send(self, **kwargs):
 
